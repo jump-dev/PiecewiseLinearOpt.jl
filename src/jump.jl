9c2b1fdaadd10f5e952e773ef3b28a46dc479cc7
Joey Huchette <joehuchette@gmail.com>
2017-03-01 20:16:43 -0500

    Updates for 2D CC and MC formulations

--- src/jump.jl
+++ src/jump.jl
@@ -131,9 +131,9 @@ end
 piecewiselinear(m::JuMP.Model, x::JuMP.Variable, y::JuMP.Variable, dˣ, dʸ, f::Function; method=defaultmethod()) =
     piecewiselinear(m, x, y, BivariatePWLFunction(dˣ, dʸ, f); method=method)
 
-function piecewiselinear(m::JuMP.Model, x::JuMP.Variable, y::JuMP.Variable, pwl::BivariatePWLFunction; method=defaultmethod())
+function piecewiselinear(m::JuMP.Model, x₁::JuMP.Variable, x₂::JuMP.Variable, pwl::BivariatePWLFunction; method=defaultmethod())
     initPWL!(m)
-    counter = m.ext[:PWL]
+    counter = m.ext[:PWL].counter
     counter += 1
     m.ext[:PWL].counter = counter
     dˣ = [_x[1] for _x in pwl.x]
@@ -142,88 +142,152 @@ function piecewiselinear(m::JuMP.Model, x::JuMP.Variable, y::JuMP.Variable, pwl:
     @assert issorted(uˣ)
     @assert issorted(uʸ)
 
+    T = pwl.T
+
     nˣ, nʸ = length(uˣ), length(uʸ)
 
     ˣtoⁱ = Dict(uˣ[i] => i for i in 1:nˣ)
-    ʸtoⁱ = Dict(uʸ[i] => i for i in 1:nʸ)
+    ʸtoʲ = Dict(uʸ[i] => i for i in 1:nʸ)
 
     fd = Array(Float64, nˣ, nʸ)
     for (v,fv) in zip(pwl.x, pwl.z)
         # i is the linear index into pwl.x...really want (i,j) pair
-        fd[ˣtoⁱ[v[1]],ʸtoⁱ[v[2]]] = fv
+        fd[ˣtoⁱ[v[1]],ʸtoʲ[v[2]]] = fv
     end
 
     z = JuMP.@variable(m, lowerbound=minimum(fd), upperbound=maximum(fd), basename="z_$counter")
-    λ = JuMP.@variable(m, [1:nˣ,1:nʸ], lowerbound=0, upperbound=1, basename="λ_$counter")
-    JuMP.@constraint(m, sum(λ) == 1)
-    JuMP.@constraint(m, sum(λ[i,j]*uˣ[i]   for i in 1:nˣ, j in 1:nʸ) == x)
-    JuMP.@constraint(m, sum(λ[i,j]*uʸ[j]   for i in 1:nˣ, j in 1:nʸ) == y)
-    JuMP.@constraint(m, sum(λ[i,j]*fd[i,j] for i in 1:nˣ, j in 1:nʸ) == z)
 
-    if method == :Logarithmic
-        for tx in 1:nˣ
-            sos2_logarthmic_formulation!(m, [λ[tx,ty] for ty in 1:nʸ])
+    if method == :MC
+        x̂₁ = JuMP.@variable(m, [T],      basename="x̂₁_$counter")
+        x̂₂ = JuMP.@variable(m, [T],      basename="x̂₂_$counter")
+        ẑ  = JuMP.@variable(m, [T],      basename="ẑ_$counter")
+        y  = JuMP.@variable(m, [T], Bin, basename="y_$counter")
+        JuMP.@constraint(m, sum(y)  == 1)
+        JuMP.@constraint(m, sum(x̂₁) == x₁)
+        JuMP.@constraint(m, sum(x̂₂) == x₂)
+        JuMP.@constraint(m, sum(ẑ)  == z)
+        for t in T
+            @assert length(t) == 3
+            r¹,  r²,  r³  = pwl.x[t[1]], pwl.x[t[2]], pwl.x[t[3]]
+            fz¹, fz², fz³ = pwl.z[t[1]], pwl.z[t[2]], pwl.z[t[3]]
+            for P in [[1,2,3],[2,3,1],[3,1,2]]
+                p¹, p², p³ = [r¹, r², r³][P]
+                p¹, p², p³ = pwl.x[t[P[1]]], pwl.x[t[P[2]]], pwl.x[t[P[3]]]
+
+                A = [p¹[1] p¹[2] 1
+                     p²[1] p²[2] 1
+                     p³[1] p³[2] 1]
+                b = [0,0,1]
+                q = A \ b
+
+                @assert isapprox(q[1]*p¹[1] + q[2]*p¹[2] + q[3], 0, atol=1e-4)
+                @assert isapprox(q[1]*p²[1] + q[2]*p²[2] + q[3], 0, atol=1e-4)
+                @assert isapprox(q[1]*p³[1] + q[2]*p³[2] + q[3], 1, atol=1e-4)
+
+                JuMP.@constraint(m, q[1]*x̂₁[t] + q[2]*x̂₂[t] + q[3]*y[t] ≥ 0)
+            end
+            A = [r¹[1] r¹[2] 1
+                 r²[1] r²[2] 1
+                 r³[1] r³[2] 1]
+            b = [fz¹, fz², fz³]
+            q = A \ b
+
+            @assert isapprox(q[1]*r¹[1] + q[2]*r¹[2] + q[3], fz¹, atol=1e-4)
+            @assert isapprox(q[1]*r²[1] + q[2]*r²[2] + q[3], fz², atol=1e-4)
+            @assert isapprox(q[1]*r³[1] + q[2]*r³[2] + q[3], fz³, atol=1e-4)
+            JuMP.@constraint(m, ẑ[t] == q[1]*x̂₁[t] + q[2]*x̂₂[t] + q[3]*y[t])
         end
-        for ty in 1:nʸ
-            sos2_logarthmic_formulation!(m, [λ[tx,ty] for tx in 1:nˣ])
-        end
-    elseif method == :CC
-        for tx in 1:nˣ
-            sos2_cc_formulation!(m, [λ[tx,ty] for ty in 1:nʸ])
-        end
-        for ty in 1:nʸ
-            sos2_cc_formulation!(m, [λ[tx,ty] for tx in 1:nˣ])
-        end
-    elseif method == :MC
-        for tx in 1:nˣ
-            sos2_mc_formulation!(m, [λ[tx,ty] for ty in 1:nʸ])
-        end
-        for ty in 1:nʸ
-            sos2_mc_formulation!(m, [λ[tx,ty] for tx in 1:nˣ])
-        end
-    else
-        error()
+        return z
     end
 
-    pattern = pwl.meta[:structure]
-    if pattern == :UnionJack
-        numT = 0
-        minˣ, minʸ = uˣ[1], uʸ[1]
-        # find the index of the bottom-left point
-        idx = findfirst(pwl.x) do w; w == (minˣ, minʸ); end
-        for t in pwl.T
-            if idx in t
-                numT += 1
-            end
-        end
-        @assert 1 <= numT <= 2
+    λ = JuMP.@variable(m, [1:nˣ,1:nʸ], lowerbound=0, upperbound=1, basename="λ_$counter")
+    JuMP.@constraint(m, sum(λ) == 1)
+    JuMP.@constraint(m, sum(λ[i,j]*uˣ[i]   for i in 1:nˣ, j in 1:nʸ) == x₁)
+    JuMP.@constraint(m, sum(λ[i,j]*uʸ[j]   for i in 1:nˣ, j in 1:nʸ) == x₂)
+    JuMP.@constraint(m, sum(λ[i,j]*fd[i,j] for i in 1:nˣ, j in 1:nʸ) == z)
 
-        w = JuMP.@variable(m, category=:Bin)
-        # If numT==1, then bottom-left is contained in only one point, and so needs separating; otherwise numT==2, and need to offset by one
-        JuMP.@constraints(m, begin
-            sum(λ[tx,ty] for tx in 1:2:nˣ, ty in    numT :2:nʸ) ≤     w
-            sum(λ[tx,ty] for tx in 2:2:nˣ, ty in (3-numT):2:nʸ) ≤ 1 - w
-        end)
-    else
-        w = JuMP.@variable(m, [0:2,0:2], Bin)
-        for oˣ in 0:2, oʸ in 0:2
-            innoT = fill(true, nˣ, nʸ)
-            for (i,j,k) in pwl.T
-                xⁱ, xʲ, xᵏ = pwl.x[i], pwl.x[j], pwl.x[k]
-                iiˣ, iiʸ = ˣtoⁱ[xⁱ[1]], ʸtoⁱ[xⁱ[2]]
-                jjˣ, jjʸ = ˣtoⁱ[xʲ[1]], ʸtoⁱ[xʲ[2]]
-                kkˣ, kkʸ = ˣtoⁱ[xᵏ[1]], ʸtoⁱ[xᵏ[2]]
-                # check to see if one of the points in the triangle falls on the grid
-                if (mod(iiˣ,3) == oˣ && mod(iiʸ,3) == oʸ) || (mod(jjˣ,3) == oˣ && mod(jjʸ,3) == oʸ) || (mod(kkˣ,3) == oˣ && mod(kkʸ,3) == oʸ)
-                    innoT[iiˣ,iiʸ] = false
-                    innoT[jjˣ,jjʸ] = false
-                    innoT[kkˣ,kkʸ] = false
+    if method == :CC
+        T = pwl.T
+        y = JuMP.@variable(m, [T], Bin, basename="y_$counter")
+        JuMP.@constraint(m, sum(y) == 1)
+
+        Ts = Dict((i,j) => Vector{Int}[] for i in 1:nˣ, j in 1:nʸ)
+        for t in T, ind in t
+            rˣ, rʸ = pwl.x[ind]
+            i, j = ˣtoⁱ[rˣ], ʸtoʲ[rʸ]
+            push!(Ts[(i,j)], t)
+        end
+        for i in 1:nˣ, j in 1:nʸ
+            JuMP.@constraint(m, λ[i,j] ≤ sum(y[t] for t in Ts[(i,j)]))
+        end
+    elseif method == :Incremental
+        #foobar
+    else # formulations with SOS2 along each dimension
+        if method == :Logarithmic
+            for tx in 1:nˣ
+                sos2_logarthmic_formulation!(m, [λ[tx,ty] for ty in 1:nʸ])
+            end
+            for ty in 1:nʸ
+                sos2_logarthmic_formulation!(m, [λ[tx,ty] for tx in 1:nˣ])
+            end
+        elseif method == :ZigZag
+            for tx in 1:nˣ
+                sos2_zigzag_formulation!(m, [λ[tx,ty] for ty in 1:nʸ])
+            end
+            for ty in 1:nʸ
+                sos2_zigzag_formulation!(m, [λ[tx,ty] for tx in 1:nˣ])
+            end
+        elseif method == :ZigZagInteger
+            for tx in 1:nˣ
+                sos2_zigzag_general_integer_formulation!(m, [λ[tx,ty] for ty in 1:nʸ])
+            end
+            for ty in 1:nʸ
+                sos2_zigzag_general_integer_formulation!(m, [λ[tx,ty] for tx in 1:nˣ])
+            end
+        else
+            error("Unrecognized method $method")
+        end
+
+        pattern = pwl.meta[:structure]
+        if pattern == :UnionJack
+            numT = 0
+            minˣ, minʸ = uˣ[1], uʸ[1]
+            # find the index of the bottom-left point
+            idx = findfirst(pwl.x) do w; w == (minˣ, minʸ); end
+            for t in pwl.T
+                if idx in t
+                    numT += 1
                 end
             end
+            @assert 1 <= numT <= 2
+
+            w = JuMP.@variable(m, category=:Bin)
+            # If numT==1, then bottom-left is contained in only one point, and so needs separating; otherwise numT==2, and need to offset by one
             JuMP.@constraints(m, begin
-                sum(λ[i,j] for i in 1+oˣ:3:nˣ, j in 1+oʸ:3:nʸ) ≤  1 - w[oˣ,oʸ]
-                sum(λ[i,j] for i in 1:nˣ, j in 1:nʸ if !innoT[i,j]) ≤ w[oˣ,oʸ]
+                sum(λ[tx,ty] for tx in 1:2:nˣ, ty in    numT :2:nʸ) ≤     w
+                sum(λ[tx,ty] for tx in 2:2:nˣ, ty in (3-numT):2:nʸ) ≤ 1 - w
             end)
+        else
+            w = JuMP.@variable(m, [0:2,0:2], Bin)
+            for oˣ in 0:2, oʸ in 0:2
+                innoT = fill(true, nˣ, nʸ)
+                for (i,j,k) in pwl.T
+                    xⁱ, xʲ, xᵏ = pwl.x[i], pwl.x[j], pwl.x[k]
+                    iiˣ, iiʸ = ˣtoⁱ[xⁱ[1]], ʸtoʲ[xⁱ[2]]
+                    jjˣ, jjʸ = ˣtoⁱ[xʲ[1]], ʸtoʲ[xʲ[2]]
+                    kkˣ, kkʸ = ˣtoⁱ[xᵏ[1]], ʸtoʲ[xᵏ[2]]
+                    # check to see if one of the points in the triangle falls on the grid
+                    if (mod(iiˣ,3) == oˣ && mod(iiʸ,3) == oʸ) || (mod(jjˣ,3) == oˣ && mod(jjʸ,3) == oʸ) || (mod(kkˣ,3) == oˣ && mod(kkʸ,3) == oʸ)
+                        innoT[iiˣ,iiʸ] = false
+                        innoT[jjˣ,jjʸ] = false
+                        innoT[kkˣ,kkʸ] = false
+                    end
+                end
+                JuMP.@constraints(m, begin
+                    sum(λ[i,j] for i in 1+oˣ:3:nˣ, j in 1+oʸ:3:nʸ) ≤  1 - w[oˣ,oʸ]
+                    sum(λ[i,j] for i in 1:nˣ, j in 1:nʸ if !innoT[i,j]) ≤ w[oˣ,oʸ]
+                end)
+            end
         end
     end
     z
