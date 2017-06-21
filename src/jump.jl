f782a4635dfe35e32730a763e470a749a901e14e
Joey Huchette <joehuchette@gmail.com>
2017-06-21 15:44:03 -0400

    Add a way to compute the optimal triangle selection

--- src/jump.jl
+++ src/jump.jl
@@ -561,10 +561,12 @@ function piecewiselinear(m::JuMP.Model, x₁::JuMP.Variable, x₂::JuMP.Variable
         for i in 1:nˣ, j in 1:nʸ
             JuMP.@constraint(m, λ[i,j] ≤ sum(y[t] for t in Ts[(i,j)]))
         end
+        return z
     elseif method == :Incremental
-        error()
+        error("Incremental formulation for bivariate functions is not currently implemented.")
     elseif method == :OptimalIB
         optimal_IB_scheme!(m, λ, pwl, subsolver)
+        return z
     else # formulations with SOS2 along each dimension
         if method == :Logarithmic
             sos2_logarthmic_formulation!(m, [sum(λ[tˣ,tʸ] for tˣ in 1:nˣ) for tʸ in 1:nʸ])
@@ -620,6 +622,84 @@ function piecewiselinear(m::JuMP.Model, x₁::JuMP.Variable, x₂::JuMP.Variable
                 sum(λ[tx,ty] for tx in 1:nˣ, ty in 1:nʸ if mod(tx,2) != mod(ty,2) && (tx+ty) in 3:4:(nˣ+nʸ)) ≤ w[2]
                 sum(λ[tx,ty] for tx in 1:nˣ, ty in 1:nʸ if mod(tx,2) != mod(ty,2) && (tx+ty) in 5:4:(nˣ+nʸ)) ≤ 1 - w[2]
             end)
+        elseif pattern == :OptimalTriangleSelection
+            if subsolver === nothing
+                error("No MIP solver provided to construct optimal triangle selection. Pass a solver object to the piecewiselinear function, e.g. piecewiselinear(m, x₁, x₂, bivariatefunc, method=:Logarithmic, subsolver=GurobiSolver())")
+            end
+            J = [(i,j) for i in 1:nˣ, j in 1:nʸ]
+            E = Set{Tuple{Tuple{Int,Int},Tuple{Int,Int}}}()
+            for t in T
+                @assert length(t) == 3
+                IJ = [(ˣtoⁱ[pwl.x[i][1]],ʸtoʲ[pwl.x[i][2]]) for i in t]
+                im = minimum(ij[1] for ij in IJ)
+                iM = maximum(ij[1] for ij in IJ)
+                jm = minimum(ij[2] for ij in IJ)
+                jM = maximum(ij[2] for ij in IJ)
+                @assert im < iM
+                @assert im < iM
+                if ((im,jm) in IJ) && ((iM,jM) in IJ)
+                    push!(E, ((im,jM),(iM,jm)))
+                elseif ((im,jM) in IJ) && ((iM,jm) in IJ)
+                    push!(E, ((im,jm),(iM,jM)))
+                else
+                    error()
+                end
+            end
+            t = 1
+            xx, yy = Array(Float64,0,0), Array(Float64,0,0)
+            while true
+                subm = JuMP.Model(solver=subsolver)
+                JuMP.@variable(subm, xˢᵘᵇ[1:t,J], Bin)
+                JuMP.@variable(subm, yˢᵘᵇ[1:t,J], Bin)
+                JuMP.@variable(subm, zˢᵘᵇ[1:t,J,J], Bin)
+
+                for j in 1:t
+                    for r in J, s in J
+                        # lexiographic ordering on points on grid
+                        (r[1] > s[1] || (r[1] == s[1] && r[2] >= s[2])) && continue
+                        JuMP.@constraints(subm, begin
+                            zˢᵘᵇ[j,r,s] <= xˢᵘᵇ[j,r] + xˢᵘᵇ[j,s]
+                            zˢᵘᵇ[j,r,s] <= xˢᵘᵇ[j,r] + yˢᵘᵇ[j,r]
+                            zˢᵘᵇ[j,r,s] <= xˢᵘᵇ[j,s] + yˢᵘᵇ[j,s]
+                            zˢᵘᵇ[j,r,s] <= yˢᵘᵇ[j,r] + yˢᵘᵇ[j,s]
+                            zˢᵘᵇ[j,r,s] >= xˢᵘᵇ[j,r] + yˢᵘᵇ[j,s] - 1
+                            zˢᵘᵇ[j,r,s] >= xˢᵘᵇ[j,s] + yˢᵘᵇ[j,r] - 1
+                        end)
+                    end
+                    for r in J
+                        JuMP.@constraint(subm, xˢᵘᵇ[j,r] + yˢᵘᵇ[j,r] <= 1)
+                    end
+                end
+
+                for r in J, s in J
+                    # lexiographic ordering on points on grid
+                    (r[1] > s[1] || (r[1] == s[1] && r[2] >= s[2])) && continue
+                    if (r,s) in E
+                        JuMP.@constraint(subm, sum(zˢᵘᵇ[j,r,s] for j in 1:t) >= 1)
+                    elseif max(abs(r[1]-s[1]), abs(r[2]-s[2])) == 1
+                        JuMP.@constraint(subm, sum(zˢᵘᵇ[j,r,s] for j in 1:t) == 0)
+                    end
+                end
+
+                JuMP.@objective(subm, Min, sum(xˢᵘᵇ) + sum(yˢᵘᵇ))
+                stat = JuMP.solve(subm)
+                if any(isnan, subm.colVal)
+                    t += 1
+                else
+                    xx = JuMP.getvalue(xˢᵘᵇ)
+                    yy = JuMP.getvalue(yˢᵘᵇ)
+                    break
+                end
+            end
+            y = JuMP.@variable(m, [1:t], Bin, basename="Δselect_$counter")
+
+            for i in 1:t
+                JuMP.@constraints(m, begin
+                    sum(λ[v[1],v[2]] for v in J if xx[i,v] ≈ 1) ≤     y[i]
+                    sum(λ[v[1],v[2]] for v in J if yy[i,v] ≈ 1) ≤ 1 - y[i]
+                end)
+            end
+            z
         else
             w = JuMP.@variable(m, [1:3,1:3], Bin, basename="w_$counter")
             for oˣ in 1:3, oʸ in 1:3
