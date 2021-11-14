dafcffbf0960929f2f34b3d9796635a7e18a7322
hellemo <hellemo@gmail.com>
2021-11-14 19:16:16 +0100

    Fix test depreciations (#44)
    * Update compat
    
    * Fix test depreciations

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,13 +2,6 @@ using Cbc
 using Test
 using LinearAlgebra
 
-# using Gurobi
-# solver = GurobiSolver(OutputFlag=0)
-
-# using CPLEX
-# solver = CplexSolver(CPX_PARAM_SCRIND=0)
-
-
 import JuMP
 import MathOptInterface
 const MOI = MathOptInterface
@@ -22,58 +15,62 @@ methods_1D = (:CC,:MC,:Logarithmic,:LogarithmicIB,:ZigZag,:ZigZagInteger,:Genera
 methods_2D = (:CC,:Logarithmic,:LogarithmicIB,:ZigZag,:ZigZagInteger,:GeneralizedCelaya,:SymmetricCelaya,:DisaggLogarithmic)
 patterns_2D = (:Upper,:Lower,:BestFit,:UnionJack,:K1,:Random) # :OptimalTriangleSelection and :Stencil not supported currently
 
-println("\nunivariate tests")
-@testset "1D: $method" for method in methods_1D
-    model = JuMP.Model(JuMP.with_optimizer(Cbc.Optimizer))
-    JuMP.@variable(model, x)
-    z = piecewiselinear(model, x, range(1,stop=2π, length=8), sin, method=method)
-    JuMP.@objective(model, Max, z)
+optimizer=JuMP.optimizer_with_attributes(Cbc.Optimizer, MOI.Silent() => true)
 
-    JuMP.optimize!(model)
+@testset "Univariate tests" begin
+    @testset "1D: $method" for method in methods_1D
+        model = JuMP.Model(optimizer)
+        JuMP.@variable(model, x)
+        z = piecewiselinear(model, x, range(1,stop=2π, length=8), sin, method=method)
+        JuMP.@objective(model, Max, z)
 
-    @test JuMP.termination_status(model) == MOI.OPTIMAL
-    @test JuMP.value(x) ≈ 1.75474 rtol=1e-4
-    @test JuMP.value(z) ≈ 0.98313 rtol=1e-4
+        JuMP.optimize!(model)
 
-    JuMP.@constraint(model, x ≤ 1.5z)
+        @test JuMP.termination_status(model) == MOI.OPTIMAL
+        @test JuMP.value(x) ≈ 1.75474 rtol=1e-4
+        @test JuMP.value(z) ≈ 0.98313 rtol=1e-4
 
-    JuMP.optimize!(model)
+        JuMP.@constraint(model, x ≤ 1.5z)
 
-    @test JuMP.termination_status(model) == MOI.OPTIMAL
-    @test JuMP.value(x) ≈ 1.36495 rtol=1e-4
-    @test JuMP.value(z) ≈ 0.90997 rtol=1e-4
-    @test JuMP.objective_value(model) ≈ 0.90997 rtol=1e-4
-    @test JuMP.objective_value(model) ≈ JuMP.value(z) rtol=1e-4
+        JuMP.optimize!(model)
+
+        @test JuMP.termination_status(model) == MOI.OPTIMAL
+        @test JuMP.value(x) ≈ 1.36495 rtol=1e-4
+        @test JuMP.value(z) ≈ 0.90997 rtol=1e-4
+        @test JuMP.objective_value(model) ≈ 0.90997 rtol=1e-4
+        @test JuMP.objective_value(model) ≈ JuMP.value(z) rtol=1e-4
+    end
 end
 
-println("\nbivariate tests")
-@testset "2D: $method, $pattern" for method in methods_2D, pattern in patterns_2D
-    model = JuMP.Model(JuMP.with_optimizer(Cbc.Optimizer))
-    JuMP.@variable(model, x[1:2])
-    d = range(0,stop=1,length=8)
-    f = (x1,x2) -> 2*(x1-1/3)^2 + 3*(x2-4/7)^4
-    z = piecewiselinear(model, x[1], x[2], BivariatePWLFunction(d, d, f, pattern=pattern), method=method)
-    JuMP.@objective(model, Min, z)
+@testset "Bivariate tests " begin
+    @testset "2D: $method, $pattern" for method in methods_2D, pattern in patterns_2D
+        model = JuMP.Model(optimizer)
+        JuMP.@variable(model, x[1:2])
+        d = range(0,stop=1,length=8)
+        f = (x1,x2) -> 2*(x1-1/3)^2 + 3*(x2-4/7)^4
+        z = piecewiselinear(model, x[1], x[2], BivariatePWLFunction(d, d, f, pattern=pattern), method=method)
+        JuMP.@objective(model, Min, z)
 
-    JuMP.optimize!(model)
+        JuMP.optimize!(model)
 
-    @test JuMP.termination_status(model) == MOI.OPTIMAL
-    @test JuMP.value(x[1]) ≈ 0.285714 rtol=1e-4
-    @test JuMP.value(x[2]) ≈ 0.571429 rtol=1e-4
-    @test JuMP.value(z) ≈ 0.004535 rtol=1e-3
-    @test JuMP.objective_value(model) ≈ 0.004535 rtol=1e-3
-    @test JuMP.objective_value(model) ≈ JuMP.value(z) rtol=1e-3
+        @test JuMP.termination_status(model) == MOI.OPTIMAL
+        @test JuMP.value(x[1]) ≈ 0.285714 rtol=1e-4
+        @test JuMP.value(x[2]) ≈ 0.571429 rtol=1e-4
+        @test JuMP.value(z) ≈ 0.004535 rtol=1e-3
+        @test JuMP.objective_value(model) ≈ 0.004535 rtol=1e-3
+        @test JuMP.objective_value(model) ≈ JuMP.value(z) rtol=1e-3
 
-    JuMP.@constraint(model, x[1] ≥ 0.6)
+        JuMP.@constraint(model, x[1] ≥ 0.6)
 
-    JuMP.optimize!(model)
+        JuMP.optimize!(model)
 
-    @test JuMP.termination_status(model) == MOI.OPTIMAL
-    @test JuMP.value(x[1]) ≈ 0.6 rtol=1e-4
-    @test JuMP.value(x[2]) ≈ 0.571428 rtol=1e-4
-    @test JuMP.value(z) ≈ 0.148753 rtol=1e-4
-    @test JuMP.objective_value(model) ≈ 0.148753 rtol=1e-3
-    @test JuMP.objective_value(model) ≈ JuMP.value(z) rtol=1e-3
+        @test JuMP.termination_status(model) == MOI.OPTIMAL
+        @test JuMP.value(x[1]) ≈ 0.6 rtol=1e-4
+        @test JuMP.value(x[2]) ≈ 0.571428 rtol=1e-4
+        @test JuMP.value(z) ≈ 0.148753 rtol=1e-4
+        @test JuMP.objective_value(model) ≈ 0.148753 rtol=1e-3
+        @test JuMP.objective_value(model) ≈ JuMP.value(z) rtol=1e-3
+    end
 end
 #
 # println("\nbivariate optimal IB scheme tests")
