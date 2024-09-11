4c3ba5996f7412e49fe47d5750ad2207f969f4a5
odow <o.dowson@gmail.com>
2024-09-11 13:56:27 +1200

    Use explicit imports of package dependencies

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,22 +3,51 @@
 # Use of this source code is governed by an MIT-style license that can be found
 # in the LICENSE.md file or at https://opensource.org/licenses/MIT.
 
-using Cbc
-using Test
-using LinearAlgebra
-
-import JuMP
-import MathOptInterface
-const MOI = MathOptInterface
-
 using PiecewiseLinearOpt
+using Test
 
-# TODO: Add :SOS2 to this list, once Cbc supports it
-methods_1D = (:CC,:MC,:Logarithmic,:LogarithmicIB,:ZigZag,:ZigZagInteger,:GeneralizedCelaya,:SymmetricCelaya,:Incremental,:DisaggLogarithmic)
-# TODO: Add :SOS2 to this list, once Cbc supports it
-# TODO: Add :MC to this list, Cbc (but not Gurobi) gives a different answer below, only for :MC (maybe a bug in Cbc?)
-methods_2D = (:CC,:Logarithmic,:LogarithmicIB,:ZigZag,:ZigZagInteger,:GeneralizedCelaya,:SymmetricCelaya,:DisaggLogarithmic)
-patterns_2D = (:Upper,:Lower,:BestFit,:UnionJack,:K1,:Random) # :OptimalTriangleSelection and :Stencil not supported currently
+import Cbc
+import JuMP
+import LinearAlgebra
+import MathOptInterface as MOI
+
+methods_1D = (
+    :CC,
+    :MC,
+    :Logarithmic,
+    :LogarithmicIB,
+    :ZigZag,
+    :ZigZagInteger,
+    :GeneralizedCelaya,
+    :SymmetricCelaya,
+    :Incremental,
+    :DisaggLogarithmic,
+    # :SOS2, not supported by Cbc
+)
+
+methods_2D = (
+    :CC,
+    :Logarithmic,
+    :LogarithmicIB,
+    :ZigZag,
+    :ZigZagInteger,
+    :GeneralizedCelaya,
+    :SymmetricCelaya,
+    :DisaggLogarithmic,
+    # :SOS2, not supported by Cbc
+    # TODO: Add :MC to this list, Cbc (but not Gurobi) gives a different answer
+    #       below, only for :MC (maybe a bug in Cbc?)
+)
+patterns_2D = (
+    :Upper,
+    :Lower,
+    :BestFit,
+    :UnionJack,
+    :K1,
+    :Random,
+    # :OptimalTriangleSelection not supported currently
+    # :Stencil
+)
 
 optimizer=JuMP.optimizer_with_attributes(Cbc.Optimizer, MOI.Silent() => true)
 
@@ -28,17 +57,12 @@ optimizer=JuMP.optimizer_with_attributes(Cbc.Optimizer, MOI.Silent() => true)
         JuMP.@variable(model, x)
         z = piecewiselinear(model, x, range(1,stop=2π, length=8), sin, method=method)
         JuMP.@objective(model, Max, z)
-
         JuMP.optimize!(model)
-
         @test JuMP.termination_status(model) == MOI.OPTIMAL
         @test JuMP.value(x) ≈ 1.75474 rtol=1e-4
         @test JuMP.value(z) ≈ 0.98313 rtol=1e-4
-
         JuMP.@constraint(model, x ≤ 1.5z)
-
         JuMP.optimize!(model)
-
         @test JuMP.termination_status(model) == MOI.OPTIMAL
         @test JuMP.value(x) ≈ 1.36495 rtol=1e-4
         @test JuMP.value(z) ≈ 0.90997 rtol=1e-4
@@ -55,20 +79,15 @@ end
         f = (x1,x2) -> 2*(x1-1/3)^2 + 3*(x2-4/7)^4
         z = piecewiselinear(model, x[1], x[2], BivariatePWLFunction(d, d, f, pattern=pattern), method=method)
         JuMP.@objective(model, Min, z)
-
         JuMP.optimize!(model)
-
         @test JuMP.termination_status(model) == MOI.OPTIMAL
         @test JuMP.value(x[1]) ≈ 0.285714 rtol=1e-4
         @test JuMP.value(x[2]) ≈ 0.571429 rtol=1e-4
         @test JuMP.value(z) ≈ 0.004535 rtol=1e-3
         @test JuMP.objective_value(model) ≈ 0.004535 rtol=1e-3
         @test JuMP.objective_value(model) ≈ JuMP.value(z) rtol=1e-3
-
         JuMP.@constraint(model, x[1] ≥ 0.6)
-
         JuMP.optimize!(model)
-
         @test JuMP.termination_status(model) == MOI.OPTIMAL
         @test JuMP.value(x[1]) ≈ 0.6 rtol=1e-4
         @test JuMP.value(x[2]) ≈ 0.571428 rtol=1e-4
@@ -77,7 +96,7 @@ end
         @test JuMP.objective_value(model) ≈ JuMP.value(z) rtol=1e-3
     end
 end
-#
+
 # println("\nbivariate optimal IB scheme tests")
 # @testset "2D: optimal IB, UnionJack" begin
 #     model = JuMP.Model(JuMP.with_optimizer(Cbc.Optimizer))
@@ -87,20 +106,15 @@ end
 #     f = (x,y) -> 2*(x-1/3)^2 + 3*(y-4/7)^4
 #     z = piecewiselinear(model, x, y, BivariatePWLFunction(d, d, f, pattern=:UnionJack), method=:OptimalIB, subsolver=solver)
 #     JuMP.@objective(model, Min, z)
-#
 #     JuMP.optimize!(model)
-#
 #     @test JuMP.termination_status(model) == MOI.OPTIMAL
 #     @test JuMP.value(x) ≈ 0.5 rtol=1e-4
 #     @test JuMP.value(y) ≈ 0.5 rtol=1e-4
 #     @test JuMP.value(z) ≈ 0.055634 rtol=1e-3
 #     @test getobjectivevalue(model) ≈ 0.055634 rtol=1e-3
 #     @test getobjectivevalue(model) ≈ JuMP.value(z) rtol=1e-3
-#
 #     JuMP.@constraint(model, x ≥ 0.6)
-#
 #     JuMP.optimize!(model)
-#
 #     @test JuMP.termination_status(model) == MOI.OPTIMAL
 #     @test JuMP.value(x) ≈ 0.6 rtol=1e-4
 #     @test JuMP.value(y) ≈ 0.5 rtol=1e-4
