1a0169981386886758fe034d0032c4c19c4f1f93
odow <o.dowson@gmail.com>
2024-09-11 14:13:28 +1200

    Add JuliaFormatter

--- test/runtests.jl
+++ test/runtests.jl
@@ -49,51 +49,65 @@ patterns_2D = (
     # :Stencil
 )
 
-optimizer=JuMP.optimizer_with_attributes(Cbc.Optimizer, MOI.Silent() => true)
+optimizer = JuMP.optimizer_with_attributes(Cbc.Optimizer, MOI.Silent() => true)
 
 @testset "Univariate tests" begin
     @testset "1D: $method" for method in methods_1D
         model = JuMP.Model(optimizer)
         JuMP.@variable(model, x)
-        z = piecewiselinear(model, x, range(1,stop=2π, length=8), sin, method=method)
+        z = piecewiselinear(
+            model,
+            x,
+            range(1; stop = 2π, length = 8),
+            sin;
+            method = method,
+        )
         JuMP.@objective(model, Max, z)
         JuMP.optimize!(model)
         @test JuMP.termination_status(model) == MOI.OPTIMAL
-        @test JuMP.value(x) ≈ 1.75474 rtol=1e-4
-        @test JuMP.value(z) ≈ 0.98313 rtol=1e-4
+        @test JuMP.value(x) ≈ 1.75474 rtol = 1e-4
+        @test JuMP.value(z) ≈ 0.98313 rtol = 1e-4
         JuMP.@constraint(model, x ≤ 1.5z)
         JuMP.optimize!(model)
         @test JuMP.termination_status(model) == MOI.OPTIMAL
-        @test JuMP.value(x) ≈ 1.36495 rtol=1e-4
-        @test JuMP.value(z) ≈ 0.90997 rtol=1e-4
-        @test JuMP.objective_value(model) ≈ 0.90997 rtol=1e-4
-        @test JuMP.objective_value(model) ≈ JuMP.value(z) rtol=1e-4
+        @test JuMP.value(x) ≈ 1.36495 rtol = 1e-4
+        @test JuMP.value(z) ≈ 0.90997 rtol = 1e-4
+        @test JuMP.objective_value(model) ≈ 0.90997 rtol = 1e-4
+        @test JuMP.objective_value(model) ≈ JuMP.value(z) rtol = 1e-4
     end
 end
 
 @testset "Bivariate tests " begin
-    @testset "2D: $method, $pattern" for method in methods_2D, pattern in patterns_2D
+    @testset "2D: $method, $pattern" for method in methods_2D,
+        pattern in patterns_2D
+
         model = JuMP.Model(optimizer)
         JuMP.@variable(model, x[1:2])
-        d = range(0,stop=1,length=8)
-        f = (x1,x2) -> 2*(x1-1/3)^2 + 3*(x2-4/7)^4
-        z = piecewiselinear(model, x[1], x[2], BivariatePWLFunction(d, d, f, pattern=pattern), method=method)
+        d = range(0; stop = 1, length = 8)
+        f = (x1, x2) -> 2 * (x1 - 1 / 3)^2 + 3 * (x2 - 4 / 7)^4
+        z = piecewiselinear(
+            model,
+            x[1],
+            x[2],
+            BivariatePWLFunction(d, d, f; pattern = pattern);
+            method = method,
+        )
         JuMP.@objective(model, Min, z)
         JuMP.optimize!(model)
         @test JuMP.termination_status(model) == MOI.OPTIMAL
-        @test JuMP.value(x[1]) ≈ 0.285714 rtol=1e-4
-        @test JuMP.value(x[2]) ≈ 0.571429 rtol=1e-4
-        @test JuMP.value(z) ≈ 0.004535 rtol=1e-3
-        @test JuMP.objective_value(model) ≈ 0.004535 rtol=1e-3
-        @test JuMP.objective_value(model) ≈ JuMP.value(z) rtol=1e-3
+        @test JuMP.value(x[1]) ≈ 0.285714 rtol = 1e-4
+        @test JuMP.value(x[2]) ≈ 0.571429 rtol = 1e-4
+        @test JuMP.value(z) ≈ 0.004535 rtol = 1e-3
+        @test JuMP.objective_value(model) ≈ 0.004535 rtol = 1e-3
+        @test JuMP.objective_value(model) ≈ JuMP.value(z) rtol = 1e-3
         JuMP.@constraint(model, x[1] ≥ 0.6)
         JuMP.optimize!(model)
         @test JuMP.termination_status(model) == MOI.OPTIMAL
-        @test JuMP.value(x[1]) ≈ 0.6 rtol=1e-4
-        @test JuMP.value(x[2]) ≈ 0.571428 rtol=1e-4
-        @test JuMP.value(z) ≈ 0.148753 rtol=1e-4
-        @test JuMP.objective_value(model) ≈ 0.148753 rtol=1e-3
-        @test JuMP.objective_value(model) ≈ JuMP.value(z) rtol=1e-3
+        @test JuMP.value(x[1]) ≈ 0.6 rtol = 1e-4
+        @test JuMP.value(x[2]) ≈ 0.571428 rtol = 1e-4
+        @test JuMP.value(z) ≈ 0.148753 rtol = 1e-4
+        @test JuMP.objective_value(model) ≈ 0.148753 rtol = 1e-3
+        @test JuMP.objective_value(model) ≈ JuMP.value(z) rtol = 1e-3
     end
 end
 
