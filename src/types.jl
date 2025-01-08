3fbcfe6b9864f9810ad0f956d7845c8c28aa5f0c
Truls Flatberg <Truls.Flatberg@sintef.no>
2025-01-08 14:00:36 +0100

    Add field for segment structure and constructors to match old interface

--- src/types.jl
+++ src/types.jl
@@ -30,17 +30,99 @@ struct SegmentHyperplaneRep{D,F} <: Segment{D,F}
     funcs::NTuple{F, AffineFunction{D}}
 end
 
+abstract type SegmentStructure{D} end
+
+struct Intervals <: SegmentStructure{1} end
+
+abstract type GridTriangulation <: SegmentStructure{2} end
+struct UnstructuredTriangulation <: GridTriangulation end
+struct K1Triangulation <: GridTriangulation end
+struct UnionJackTriangulation <: GridTriangulation end
+
+
 struct PWLFunction{D, F, T <: Segment{D, F}}
     segments::Vector{T}
-    meta::Dict
-
-    function PWLFunction{D, F, T}(segments::Vector{T}) where {D, F, T <: Segment}
-        return new(segments, Dict())
-    end
+    structure::SegmentStructure{D}
 end
 
 const PWLFunctionPointRep{D, F} = PWLFunction{D, F, SegmentPointRep{D, F}}
 const PWLFunctionHyperplaneRep{D, F} = PWLFunction{D, F, SegmentHyperplaneRep{D, F}}
 
-const UnivariatePWLFunction{F} = PWLFunctionPointRep{1, F}
-const BivariatePWLFunction{F} = PWLFunctionPointRep{2, F}
+#const UnivariatePWLFunction{F} = PWLFunctionPointRep{1, F}
+#const BivariatePWLFunction{F} = PWLFunctionPointRep{2, F}
+
+const UnivariatePWLFunction = PWLFunctionPointRep{1, 1}
+const BivariatePWLFunction = PWLFunctionPointRep{2, 1}
+
+function PWLFunctionPointRep{1,1}(x::Vector, z::Vector)
+
+    if length(x) != length(z)
+        error("Mismatch in the number of points and function values")
+    end
+    xs = [convert(Float64, xi) for xi in x]
+    zs = [convert(Float64, zi) for zi in z]
+    segments = [PiecewiseLinearOpt.SegmentPointRep{1,1}([(xs[i],), (xs[i+1],)], [(zs[i],), (zs[i+1],)]) for i in 1:length(x)-1]
+
+    return UnivariatePWLFunction(segments, Intervals())
+end
+
+function PWLFunctionPointRep{1,1}(x::Vector, fz::Function)
+    z = [fz(xi) for xi in x]
+    return UnivariatePWLFunction(x, z)
+end
+
+function PWLFunctionPointRep{2,1}(
+    x,
+    y,
+    fz::Function;
+    pattern = :K1,
+    seed = hash((length(x), length(y))),
+)
+    xs = [convert(Float64, xi) for xi in x]
+    ys = [convert(Float64, yi) for yi in y]
+
+    segments = SegmentPointRep{2,1}[]
+    structure = UnstructuredTriangulation()
+    if pattern == :K1
+        structure = K1Triangulation()
+    elseif pattern == :UnionJack
+        structure = UnionJackTriangulation()
+    end
+
+    mt = Random.MersenneTwister(seed)
+
+    # run for each square on [x[i],x[i+1]] × [y[i],y[i+1]]
+    for i in 1:length(xs)-1, j in 1:length(ys)-1
+        xL, xU, yL, yU = xs[i], xs[i+1], ys[j], ys[j+1]
+        mid1 = 0.5 * (fz(xL, yL) + fz(xU, yU))
+        mid2 = 0.5 * (fz(xL, yU) + fz(xU, yL))
+        mid3 = fz(0.5 * (xL + xU), 0.5 * (yL + yU))
+        diagonal_nw_se = true
+        if pattern == :Upper
+            diagonal_nw_se = (mid1 > mid2)
+        elseif pattern == :Lower
+            diagonal_nw_se = (mid1 < mid2)
+        elseif pattern == :BestFit
+            diagonal_nw_se = (abs(mid1 - mid3) < abs(mid2 - mid3))
+        elseif pattern == :K1
+            diagonal_nw_se = false
+        elseif pattern == :UnionJack
+            diagonal_nw_se = isodd(i + j)
+        elseif pattern == :Random
+            diagonal_nw_se = rand(mt, Bool)
+        end
+
+        if diagonal_nw_se
+            corners1 = [(xL, yL), (xL, yU), (xU, yL)] # SW, NW, SE
+            corners2 = [(xU, yL), (xL, yU), (xU, yU)] # SE, NW, NE
+        else
+            corners1 = [(xL, yL), (xL, yU), (xU, yL)] # SW, NW, SE
+            corners2 = [(xU, yL), (xL, yU), (xU, yU)] # SE, NW, NE
+        end
+
+        push!(segments, SegmentPointRep{2,1}(corners1, [(fz(c...),) for c in corners1]))
+        push!(segments, SegmentPointRep{2,1}(corners2, [(fz(c...),) for c in corners2]))
+    end
+
+    return BivariatePWLFunction(segments, structure)
+end
